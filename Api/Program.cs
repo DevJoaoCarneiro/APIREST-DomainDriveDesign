95bc7b7347e9fb32f64bed784a3c87d43e634273
Joao Victor <184165528+DevJoaoCarneiro@users.noreply.github.com>
2026-01-13 21:46:08 -0300

    Rollback testes funcionais

--- Api/Program.cs
+++ Api/Program.cs
@@ -75,5 +75,3 @@ app.MapControllers();
 
 app.Run();
 
-public partial class Program { }
-
--- Tests.E2E/Support/TestServerFixture.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Tests.E2E.Support;
-
-public static class TestServerFixture
-{
-    public static readonly TestWebFactory Factory = new();
-}
